c0600ce63447f1a93e2e2a5373bdc1026a6a53cb
Fuyao Zhao <zfy0701@gmail.com>
2024-03-04 18:58:36 -0800

    script

--- push-image.sh
+++ push-image.sh
@@ -3,5 +3,5 @@
 export DOCKER_DEFAULT_PLATFORM=linux/amd64
 
 make build-docker
-docker tag zkevm-node:latest ${SENTIO_DOCKER_REPO}/zkevm-node:develop
-docker push ${SENTIO_DOCKER_REPO}/zkevm-node:develop
+docker tag zkevm-node:latest ${SENTIO_DOCKER_REPO}/cdk-validium-node:v0.5.12-CDK2
+docker push ${SENTIO_DOCKER_REPO}/cdk-validium-node:v0.5.12-CDK2
