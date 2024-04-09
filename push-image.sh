90b0b24702f44a63616376f9a9bf46aff546bca1
Fuyao Zhao <zfy0701@gmail.com>
2024-04-08 23:19:07 -0700

    update to cdk8

--- push-image.sh
+++ push-image.sh
@@ -3,5 +3,5 @@
 export DOCKER_DEFAULT_PLATFORM=linux/amd64
 
 make build-docker
-docker tag zkevm-node:latest ${SENTIO_DOCKER_REPO}/cdk-validium-node:v0.5.13-CDK2
-docker push ${SENTIO_DOCKER_REPO}/cdk-validium-node:v0.5.13-CDK2
+docker tag zkevm-node:latest ${SENTIO_DOCKER_REPO}/cdk-validium-node:v0.5.13-cdk.8
+docker push ${SENTIO_DOCKER_REPO}/cdk-validium-node:v0.5.13-cdk.8
