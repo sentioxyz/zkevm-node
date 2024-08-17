4a8551a92f316280286355ce23247bebf62cc616
Fuyao Zhao <zfy0701@gmail.com>
2024-08-17 10:43:23 -0700

    improve push script

--- push-image.sh
+++ push-image.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 export DOCKER_DEFAULT_PLATFORM=linux/amd64
+branch=$(git symbolic-ref --short HEAD)
+arr=(${branch//\// })
+version=${arr[1]}
+
+echo "build for $version"
 
 make build-docker
-docker tag zkevm-node:latest ${SENTIO_DOCKER_REPO}/zkevm-node:v0.7.0
-docker push ${SENTIO_DOCKER_REPO}/zkevm-node:v0.7.0
+docker tag zkevm-node:latest ${SENTIO_DOCKER_REPO}/zkevm-node:$version
+docker push ${SENTIO_DOCKER_REPO}/zkevm-node:$version
